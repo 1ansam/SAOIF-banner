901337597795c79c5f74e56d2e5d6bbc41f242e4
Sam Yan <1anxuf@gmail.com>
2022-07-25 10:14:48 +0800

    upload
    repair some issue to this macro

--- Boss.vb
+++ Boss.vb
@@ -1,60 +1,102 @@
-Dim i=0
-//iphone 6;6s;7;8;se;se2;se3
-//Dim pixel = 1
-//iphone 7p 8p
-Dim pixel = 1.44
+Dim width,height
 
+width = GetScreenX()
+
+height = GetScreenY()
+
+Dim scaleX, scaleY
+
+scaleX = width / 1334
+
+scaleY = height / 750
+
+Tap 50, 643
+
+Delay 1000
+
+Tap 204,810
+
+Delay 4000
 
 
 Do
-	Dim greyOkX, greyOkY
-FindColor 154*pixel, 623*pixel, 197*pixel, 668*pixel, "767676", 4, 0.90, greyOkX, greyOkY
-//如果没有找到，intX和intY的值都会被置为-1
-If greyOkX > -1 And greyOkY > -1 Then
-For i = 1 To 99
-Dim healthX, healthY
-    FindColor 677*pixel, 119*pixel, 698*pixel, 239*pixel, "46E17D", 4, 0.90, healthX, healthY
-    If healthX > -1 And healthY > -1 Then
-    Tap 175*pixel, 643*pixel
-        Delay 1000
-        Exit For
-    End If
-
-
-Next
-End If
-For i = 1 To 99
-Dim blueOkX, blueOkY
-	FindColor 171*pixel, 717*pixel, 239*pixel, 779*pixel, "D3AC57", 4, 0.90, blueOkX, blueOkY
-	If blueOkX > -1 And blueOkY > -1 Then
-        Tap 202*pixel, 748*pixel
-            Delay 1000
-            Exit For
-    End If
-    Delay 4000        
-Next
-
-
-For i = 1 To 99
-Dim healthInX, healthInY
-	FindColor 677*pixel, 119*pixel, 698*pixel, 239*pixel, "46E17D", 4, 0.90, healthInX, healthInY
-	If healthInX > -1 And healthInY > -1 Then 
-		Delay 2000
-		Tap 66*pixel, 1165*pixel
-		For i = 1 To 999
-			Delay 1000
-			Tap 382*pixel, 1249*pixel
-			Dim blackOutX, blackOutY
-			FindColor 677*pixel, 119*pixel, 698*pixel, 239*pixel, "000000", 0, 1.00, blackOutX, blackOutY
-			If blackOutX > -1 And blackOutY > -1 Then 
-				Exit For
-			End If
-			Delay 1000
-		Next
+For i = 1 To 1000
+	TracePrint i
+    Dim health1X,health1Y
+	FindMultiColor 679,122,696,391,"46DE7C","",0,0.9,health1X,health1Y
+	If health1X > -1 And health1Y > -1 Then
 		Exit For
-		
 	End If
-	Delay 1000
+	Delay 100
+	
 Next
-Delay 15000
+
+Delay 200
+
+Tap 60, 1168
+
+Delay 500
+
+Tap 60, 1168
+
+Delay 200
+
+
+For i = 1 To 1000
+    Tap 386, 1246
+    Delay 1000
+	Dim OK1X,OK1Y
+	FindMultiColor 679,122,696,391,"46DE7C","",0,0.9,OK1X,OK1Y
+	If OK1X <= -1 And OK1Y <= -1 Then
+		Exit For
+	End If
+    
+Next
+
+Delay 3000
+
+
+
+For i = 1 To 1000
+	TracePrint i
+    Dim health2X,health2Y
+	FindMultiColor 679,122,696,391,"46DE7C","",0,0.9,health2X,health2Y
+	If health2X > -1 And health2Y > -1 Then
+		Exit For
+	End If
+	Delay 100
+	
+Next
+
+Delay 4000
+
+For i = 1 To 1000
+	Dim OK2X,OK2Y
+	FindColor 167,638,182,652,"767676",0,0.9,OK2X,OK2Y
+	If OK2X > -1 And OK2Y > -1 Then
+		Exit For
+	End If
+	Delay 100
+    
+Next
+
+Tap 175, 645
+
+Delay 1000
+
+Tap 203, 807
+
+Delay 4000
 Loop
+
+
+
+
+
+
+
+
+
+
+
+
